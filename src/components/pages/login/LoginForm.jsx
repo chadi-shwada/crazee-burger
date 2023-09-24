0a95addf6475baa112e65155742e7e161208a7de
chadi-shwada <chadidu93@gmail.com>
2023-09-24 16:20:03 +0200

    💄 F03 > LOGIN > LoginForm > button > Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { theme } from "../../../theme";
 import { BsPersonCircle } from "react-icons/bs";
+import { IoChevronForwardCircle } from "react-icons/io5";
 
 export default function LoginForm() {
   // state
@@ -27,6 +28,7 @@ export default function LoginForm() {
       <hr />
       <br />
       <h2>Connectez-vous</h2>
+
       <div className="input-with-icon">
         <BsPersonCircle className="icon" />
         <input
@@ -37,7 +39,11 @@ export default function LoginForm() {
           placeholder="Entrez votre prénom"
         />
       </div>
-      <button>Accéder à votre espace</button>
+
+      <button className="btn-with-icon">
+        <span>Accéder à votre espace</span>
+        <IoChevronForwardCircle className="icon" />
+      </button>
     </LoginFormStyled>
   );
 }
@@ -68,6 +74,7 @@ const LoginFormStyled = styled.form`
     margin-bottom: 40px;
   }
 
+  // style for input
   .input-with-icon {
     background-color: #fff;
     border-radius: 5px;
@@ -92,6 +99,51 @@ const LoginFormStyled = styled.form`
     &::placeholder {
       background: white;
       color: lightgrey;
+    }
+  }
+
+  // style for button
+  .btn-with-icon {
+    background-color: ${theme.colors.primary};
+    border-radius: 5px;
+    align-items: center;
+    padding: 18px 24px;
+    font-size: 15px;
+    font-weight: 800;
+    width: 100%;
+    color: white;
+    display: inline-flex;
+    justify-content: center;
+    position: relative;
+    text-decoration: none;
+    line-height: 1;
+    border: 1px solid ${theme.colors.primary};
+    white-space: nowrap;
+
+    &:hover:not(:disabled) {
+      background-color: white;
+      color: ${theme.colors.primary};
+      border: 1px solid ${theme.colors.primary};
+      transition: all 200ms ease-out;
+    }
+
+    &:active {
+      color: white;
+      background-color: ${theme.colors.primary};
+      border: ${theme.colors.primary};
+    }
+
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+
+    .icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 15px;
+      margin-left: 10px;
     }
   }
 `;
