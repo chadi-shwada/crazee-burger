5ac7e79570a0eb80d5b5a9f495e4c306181e477f
chadi-shwada <chadidu93@gmail.com>
2023-09-29 20:00:34 +0200

    💄  F04: ORDER > add logo & profile

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -4,15 +4,29 @@ import { styled } from "styled-components";
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
-      <span>Bonjour {username}</span>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+      <div className="logo">LOGO</div>
+      <div className="profile">
+        <span>Bonjour {username}</span>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
     </NavbarStyled>
   );
 }
+
 const NavbarStyled = styled.nav`
   border: 3px solid red;
   height: 10vh;
   background: green;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    background: purple;
+  }
+
+  .profile {
+    background: pink;
+  }
 `;
