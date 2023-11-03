73b43568fca139f9228736dc43a868f4dad42126
chadi-shwada <chadidu93@gmail.com>
2023-11-03 21:01:52 +0100

    ✨ F07: ADMIN > Tab >Add label

--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { styled } from "styled-components";
 import { theme } from "../../theme";
 
-export default function Tab({ Icon, onClick, className }) {
+export default function Tab({ label, Icon, onClick, className }) {
   return (
     <TabStyled onClick={onClick} className={className}>
       <div className="icon">{Icon}</div>
+      {label && <span className="label">{label}</span>}
     </TabStyled>
   );
 }
@@ -44,5 +45,9 @@ const TabStyled = styled.button`
 
   .icon {
     display: flex;
+  }
+
+  .label {
+    margin-left: 13px;
   }
 `;
