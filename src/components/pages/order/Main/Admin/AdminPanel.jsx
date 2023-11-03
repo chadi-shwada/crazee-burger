d536a624882836a997fd8b47234e631eff1fa4aa
chadi-shwada <chadidu93@gmail.com>
2023-11-03 16:05:47 +0100

    ✨ F07: ADMIN > Add AdminTabs & AdminPanel

---
+++ src/components/pages/order/Main/Admin/AdminPanel.jsx
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+import { theme } from "../../../../../theme";
+
+export default function AdminPanel() {
+  return <AdminPanelStyled>AdminPanelStyled</AdminPanelStyled>;
+}
+
+const AdminPanelStyled = styled.div`
+  height: 250px;
+  border: 1px solid ${theme.colors.greyLight};
+  background: ${theme.colors.white};
+  box-shadow: ${theme.shadows.subtle};
+`;
